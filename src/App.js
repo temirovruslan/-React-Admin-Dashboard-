bafb8f8d7dcabab04b3678a0018473a9d5d33e12
Temirov Ruslan <ruslan.temirov1995@gmail.com>
2022-09-14 15:30:38 +0300

    added-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from './pages/login/Login'
+import New from './pages/new/New'
+import Single from './pages/single/Single'
+import List from './pages/list/List'
+
+import { BrowserRouter , Routes, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			 <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New  title="Add New User" />}/>
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New  title="Add New Product" />}/>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
