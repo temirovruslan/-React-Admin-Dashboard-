e52756a4ecdc352aaa2ac3dba1e7ac14910dcc1b
Temirov Ruslan <ruslan.temirov1995@gmail.com>
2022-09-17 13:35:00 +0300

    added-wingit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import Sidebar from '../../components/sidebar/Sidebar'
-import './Home.scss'
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
+import "./Home.scss";
 
 const Home = () => {
-  return (
-    <div className='home'>
-      <Sidebar/>
-      <div className="homeContainer">
-      <Navbar/>
-        homecontainer
-      </div>
-        
-    </div>
-  )
-}
+	return (
+		<div className="home">
+			<Sidebar />
+			<div className="homeContainer">
+				<Navbar />
+				<div className="widgets">
+					<Widget type="user"/>
+					<Widget type="order"/>
+					<Widget type="earning"/>
+					<Widget type="balance"/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
