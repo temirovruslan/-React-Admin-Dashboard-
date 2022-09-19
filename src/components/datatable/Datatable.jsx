6a4f2769ee12223eb42e5a9c8cd3322a5bb718ca
Temirov Ruslan <ruslan.temirov1995@gmail.com>
2022-09-19 14:59:26 +0300

    added-link-for-pages

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -18,7 +18,10 @@ const Datatable = () => {
 			renderCell: () => {
 				return (
 					<div className="cellAction">
-						<div className="viewButton">View</div>
+						<Link to="/users/test">
+							<div className="viewButton">View</div>
+						</Link>
+
 						<div className="deleteButton">Delete</div>
 					</div>
 				);
@@ -28,6 +31,12 @@ const Datatable = () => {
 
 	return (
 		<div className="datatable">
+			<div className="datatableTitle">
+				Add New User
+				<Link to="/users/new" className="link">
+					Add New
+				</Link>
+			</div>
 			<DataGrid
 				rows={userRows}
 				columns={userColumns.concat(actionColumn)}
