102806a2bef221e669fb6a9805bab8948de2b2bc
Temirov Ruslan <ruslan.temirov1995@gmail.com>
2022-09-20 10:36:27 +0300

    added-log-in-page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Login.scss'
 
 const Login = () => {
+  const[error,setError] = useState(false)
+
   return (
-    <div>Login</div>
+    <div className='login'>
+      <form >
+        <input type="text" placeholder='email'/>
+        <input type="password" placeholder='password'/>
+        <button type='submit'>Log in</button>
+       {error && <span>Wrong email or password</span>}
+      </form>
+    </div>
   )
 }
 
