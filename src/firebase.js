b27da6ae153933b1d15bcb66342644a7c02cf0ad
Temirov Ruslan <ruslan.temirov1995@gmail.com>
2022-09-20 11:28:23 +0300

    added-login-components

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,12 @@
 
 import { initializeApp } from "firebase/app";
-
+import { getAuth} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  // apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  apiKey: 'AIzaSyC85M0fOsL5BIzJ8MOdCG-ygBnezG7ilD8',
   authDomain: "tutorial-b3950.firebaseapp.com",
   projectId: "tutorial-b3950",
   storageBucket: "tutorial-b3950.appspot.com",
@@ -13,3 +16,6 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
+export const db = getFirestore(app);
+export const storage = getStorage(app);
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,102 @@
-import React, { useState } from 'react'
-import './Login.scss'
+import React, { useState } from "react";
+import "./Login.scss";
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const[error,setError] = useState(false)
+	const [error, setError] = useState(false);
 
-  return (
-    <div className='login'>
-      <form >
-        <input type="text" placeholder='email'/>
-        <input type="password" placeholder='password'/>
-        <button type='submit'>Log in</button>
-       {error && <span>Wrong email or password</span>}
-      </form>
-    </div>
-  )
-}
+	const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
 
-export default Login
+  const navitage = useNavigate()
+
+	const handleLogin = (e) => {
+		e.preventDefault();
+
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				const user = userCredential.user;
+				navitage('/')
+			})
+			.catch((error) => {
+				setError(error)
+			});
+	};
+
+	return (
+		<div className="login">
+			<form onSubmit={handleLogin}>
+				<input
+					type="email"
+					placeholder="email"
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<input
+					type="password"
+					placeholder="password"
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<button type="submit">Log in</button>
+				{error && <span>Wrong email or password</span>}
+			</form>
+		</div>
+	);
+};
+
+export default Login;
+
+// import { useContext, useState } from "react";
+// import "./login.scss";
+// import { signInWithEmailAndPassword } from "firebase/auth";
+// import { auth } from "../../firebase";
+// import { useNavigate } from "react-router-dom";
+// import {AuthContext} from "../../context/AuthContext"
+
+// const Login = () => {
+//   const [error, setError] = useState(false);
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const navitage = useNavigate()
+
+//   const {dispatch} = useContext(AuthContext)
+
+//   const handleLogin = (e) => {
+//     e.preventDefault();
+
+//     signInWithEmailAndPassword(auth, email, password)
+//       .then((userCredential) => {
+//         // Signed in
+//         const user = userCredential.user;
+//         dispatch({type:"LOGIN", payload:user})
+//         navitage("/")
+//       })
+//       .catch((error) => {
+//         setError(true);
+//       });
+//   };
+
+//   return (
+//     <div className="login">
+//       <form onSubmit={handleLogin}>
+//         <input
+//           type="email"
+//           placeholder="email"
+//           onChange={(e) => setEmail(e.target.value)}
+//         />
+//         <input
+//           type="password"
+//           placeholder="password"
+//           onChange={(e) => setPassword(e.target.value)}
+//         />
+//         <button type="submit">Login</button>
+//         {error && <span>Wrong email or password!</span>}
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Login;
